61e17aed50be3237c6f88bd07078cbec8b3b028b
Gautam Anand <GautamAnand@JohnDeere.com>
2020-03-05 18:03:32 +0530

    final changes

--- server.js
+++ server.js
@@ -4,98 +4,96 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex=require('knex');
 
-const postgres=knex({
+const db=knex({
     client: 'pg',
     connection: {
       host : 'smartbrain.cedooml21cmy.ap-south-1.rds.amazonaws.com',
       user : 'postgres',
       password : 'anand9911',
-      database:null
+      database:'smartbrain'
     }
   });
-
-  console.log(postgres.select('*').from('users').then(data=>{
-      console.log(data)
-  }))
-
-
+  
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'jhon',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(database)
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-        // res.json('success');
-    }
-    else {
-        res.status(400).json('Error logging in');
-    }
+    db.select('email','hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data=>{
+        const isValid=bcrypt.compareSync(req.body.password,data[0].hash)
+        if(isValid){
+            return db.select('*').from('users')
+            .where('email','=',req.body.email)
+            .then(user=>{
+                res.json(user[0])
+            })
+            .catch(err=>res.status(400).json('unable to get the user'))
+        }
+        else{
+            res.status(400).json('Wrong Credentials')
+        }
+    })
+    .catch(err=>res.status(400).json('Wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function (err, hash) {
-        console.log(hash);
-    });
+    const hash=bcrypt.hashSync(password);
+    db.transaction(trx=>{
+        trx.insert({
+            hash:hash,
+            email:email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail=>{
+         return trx('users')
+            .returning('*')
+            .insert({
+                email:loginEmail[0],
+                name:name,
+                joined:new Date()
+            }).then(users=>{
+                res.json(users[0]);
+            })
 
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    res.json(database.users[database.users.length - 1]);
+    .catch(err=>res.status(400).json("Unable to Register")) 
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+    .then(user=>{
+        if (user.length){
+        res.json(user[0])
+        }else{
+            res.status(400).json('Not Found')
         }
     })
-    if (!found) {
-        res.status(404).json('user not found')
-    }
+    .catch(err=>res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
 
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id','=',id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries=>{
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(404).json('user not found')
-    }
-
+    .catch(err=>res.status(400).json('Unable to get entries'))
 })
 
 
